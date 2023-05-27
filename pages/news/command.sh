3ee98db6090b578511907d0911e0c82744691dcf
arcchang1236 <f22893837@gmail.com>
2023-05-27 22:30:30 +0800

    Generate news automately - 2023.05.27

--- pages/news/command.sh
+++ pages/news/command.sh
@@ -2,6 +2,7 @@ cd /c/Users/User/Desktop/code/arcchang1236.github.io/pages/news
 python generate_news.py
 
 cd /c/Users/User/Desktop/code/arcchang1236.github.io
+git config --global credential.helper store
 git add .
 git commit -am "Generate news automately - $(date +"%Y.%m.%d")"
 ping www.github.com && git push origin --all || echo "not connected"
